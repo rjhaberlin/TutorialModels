5780f321ba52b6205c4042895f3e7cf9f36995b1
rjhaberlin <rjhaberlin@comcast.net>
2014-11-21 21:38:23 -0500

    Eclipse models
    These are the first tutorial models I built in Eclipse

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+'''
+Created on Nov 21, 2014
+
+@author: Rick
+'''
+# the sum of two elements defines the next
+a, b = 0, 1
+while b<10:
+    print b
+    a, b = b, a+b
+    
+#stopped at 4.2 for statements
---
+++ MyFirstModel.py
@@ -0,0 +1,12 @@
+'''
+Created on Nov 20, 2014
+
+@author: Rick
+'''
+the_world_is_flat = 1
+if the_world_is_flat:
+    print "Be careful not to fall off!"
+    
+spam = 1
+text = "#This is not a comment because it is inside quotes."
+print text
